a7b0a895d862558f106b3d9c7a001e59cffbbd48
mdzaif <mdizaif@gmail.com>
2024-06-18 15:58:46 +0600

    Initial upload

---
+++ collect_data.py
@@ -0,0 +1,43 @@
+#!/usr/bin python3
+
+import requests
+import re
+from bs4 import BeautifulSoup
+import pandas as pd
+
+r = requests.get('https://github.com/mdzaif?tab=repositories')
+rc = r.content
+s = BeautifulSoup(rc, 'html.parser') # prasering with html type of the content from the request.
+
+q = s.find_all('li', {'class' : 'col-12 d-flex flex-justify-between width-full py-4 border-bottom color-border-muted public source'})
+#q1 = s.find_all('div', {'class' : 'js-profile-editable-replace'})
+#dct['Name'] = t.find('span', {'itemprop' : 'name'}).text.strip()
+#dct['User Name'] = t.find('span', {'itemprop' : 'additionalName'}).text.strip()
+
+
+li = []
+for repo in q:
+    dct = {}
+    dct['Repository Name'] = repo.find('a', {'itemprop' : 'name codeRepository'}).text.strip()
+    if(repo.find('span', {'itemprop' : 'programmingLanguage'})):
+        dct['Programming Language'] = repo.find('span', {'itemprop' : 'programmingLanguage'}).text.strip()
+    else:
+        dct['Programming Language'] = "No Programming Language Found"
+    if(repo.find('p', {'class': 'col-9 d-inline-block color-fg-muted mb-2 pr-4'})):
+        dct['Description'] = repo.find('p', {'class': 'col-9 d-inline-block color-fg-muted mb-2 pr-4'}).text.strip()
+    else:
+        dct['Description'] = "No Description Found"
+    if(repo.find('a', {'class': 'Link--muted mr-3'})):
+        dct['Total Star'] = repo.find('a', {'class': 'Link--muted mr-3'}).text.strip()
+    else:
+        dct['Total Star'] = '0'
+    ht = repo.find('a')
+    ht = ht.get('href')
+    ht = 'https://github.com'+ht
+    dct['Repository Link'] = ht
+    li.append(dct)
+
+df = pd.DataFrame(li)
+df.to_csv('list_of_project.csv')
+print("process completed!")
+    
